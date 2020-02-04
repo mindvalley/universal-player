9adc7308d03a1124bd72a92bb4c2ab3dc7a4a68f
Duane Adam <contact@duaneadam.me>
2020-02-04 14:58:17 +0800

    Add video markers

--- examples/kitchensink/App.vue
+++ examples/kitchensink/App.vue
@@ -3,20 +3,41 @@
     <mindvalley-universal-player
       :sources="sources"
       :posterUrl="posterUrl"
+      :markers="markers"
+    />
+
+    <br/>
+    <br/>
+
+    <mindvalley-universal-player
+      :sources="sources"
+      :posterUrl="posterUrl"
     />
   </div>
 </template>
 
 <script>
 export default {
-  data() {
+  data () {
     return {
       sources: [
-        { src: '//vjs.zencdn.net/v/oceans.mp4', type: 'video/mp4' },
-        { src: '//vjs.zencdn.net/v/oceans.webm', type: 'video/webm' },
-        { src: '//vjs.zencdn.net/v/oceans.ogv', type: 'video/ogg' },
+        {
+          src: 'https://assets.mindvalley.com/api/v1/assets/9be313df-b5d6-480f-ae38-0e64ea42a577.m3u8',
+          type: 'video/hls'
+        },
+        {
+          src: 'https://assets.mindvalley.com/api/v1/assets/fa92cc13-8e44-4fe8-8516-d9ff84119841.mp4',
+          type: 'video/mp4'
+        }
       ],
-      posterUrl: '//vjs.zencdn.net/v/oceans.png',
+      markers: [
+        { text: "Welcome to Mindvalley Universal Player", time: 10 },
+        { text: "The player has a lot of features, check out the docs for reference", time: 100 },
+        { text: "Let's get started", time: 240 },
+        { text: "The documentation can be found here at https://github.com/mindvalley/universal-player", time: 1060 },
+        { text: "End", time: 5060 },
+      ],
+      posterUrl: 'https://assets.mindvalley.com/api/v1/assets/30206741-a1ed-453d-a197-7af465671466.png',
     };
   }
 };
--- src/mindvalley-universal-player-component.vue
+++ src/mindvalley-universal-player-component.vue
@@ -1,25 +1,54 @@
 <template>
-  <div>
-    <video
-      ref="player"
-      preload="auto"
-      :poster="posterUrl"
-      :controls="showControls"
-      :autoplay="autoplay"
-      class="video-js"
-    >
-      <source
-        v-for="source in sources"
-        v-bind:key="source.type"
-        :type="source.type"
-        :src="source.src"
+  <div
+    class="mv-universal-player-container"
+  >
+      <video
+        ref="video"
+        preload="auto"
+        :poster="posterUrl"
+        :controls="showControls"
+        :autoplay="autoplay"
+        :class="{ 'mv-universal-player--with-markers' : markersEnabled }"
+        class="mv-universal-player video-js"
       >
-      <p class="vjs-no-js">
-        To view this video please enable JavaScript, and consider upgrading to a web browser that
-        <a href="https://videojs.com/html5-video-support/" target="_blank"> supports HTML5 video</a>
-      </p>
-    </video>
-    <!-- <button @click="increment()">increment ({{ counter }})</button> -->
+        <source
+          v-for="source in sources"
+          v-bind:key="source.type"
+          :type="source.type"
+          :src="source.src"
+        >
+        <p class="vjs-no-js">
+          To view this video please enable JavaScript, and consider upgrading to a web browser that
+          <a href="https://videojs.com/html5-video-support/" target="_blank"> supports HTML5 video</a>
+        </p>
+      </video>
+
+      <section
+        v-if="markersEnabled"
+        class="markers-container"
+      >
+        <header class="markers-header">
+          Jump to
+        </header>
+
+        <ul
+          ref="markers"
+          class="markers"
+        >
+          <li
+            @click="jumpTo(marker.time)"
+            v-for="marker in markers"
+            class="markers__item"
+          >
+            <span class="markers__time">
+              {{ formatDuration(marker.time) }}
+            </span>
+            <span class="markers__text">
+              {{ marker.text }}
+            </span>
+          </li>
+        </ul>
+      </section>
   </div>
 </template>
 
@@ -30,6 +59,16 @@ import 'video.js/dist/video-js.css';
 export default {
   name: 'MindvalleyUniversalPlayer',
   props: {
+    mediaType: {
+      type: String,
+      default: 'video',
+      validator: function (value) {
+        return [
+          'video',
+          'audio',
+        ].indexOf(value) !== -1
+      }
+    },
     sources: {
       type: Array,
       required: true,
@@ -38,6 +77,9 @@ export default {
       type: String,
       // TODO: Placeholder that is suitable for AUDIO & VIDEO media
       default: '',
+    },
+    markers: {
+      type: Array,
     },
     showControls: {
       type: Boolean,
@@ -67,18 +109,67 @@ export default {
       default: false,
     }
   },
-  mounted() {
-    videojs(this.$refs.player, {
-      playbackRates: this.playbackRates,
-      // For reference:
-      // https://github.com/videojs/video.js/blob/master/src/js/control-bar/control-bar.js
-      controlBar: {
-        fullscreenToggle: !this.disableFullscreen,
-        playbackRateMenuButton: !this.disablePlaybackRates,
-        pictureInPictureToggle: this.enablePictureInPicture,
-      }
-    });
+  data () {
+    return {
+      player: null,
+    }
   },
+  computed: {
+    markersEnabled: function () {
+      return this.markers != null ? true : false;
+    },
+    sortedMarkers: function () {
+      return this.markersEnabled ? this.markers.sort((a, b) => a.time - b.time) : null;
+    },
+  },
+  mounted () {
+    this.initialize();
+  },
+  beforeDestroy () {
+    if (!this.player) {
+      this.initialize()
+    }
+  },
+  methods: {
+    initialize () {
+      if (this.crossOrigin !== '') {
+        this.$refs.video.crossOrigin = this.crossOrigin
+        this.$refs.video.setAttribute('crossOrigin', this.crossOrigin)
+      }
+
+      const self = this;
+
+      this.player = videojs(this.$refs.video, {
+        playbackRates: this.playbackRates,
+        fluid: true,
+        // For reference:
+        // https://github.com/videojs/video.js/blob/master/src/js/control-bar/control-bar.js
+        controlBar: {
+          fullscreenToggle: !this.disableFullscreen,
+          playbackRateMenuButton: !this.disablePlaybackRates,
+          pictureInPictureToggle: this.enablePictureInPicture,
+        }
+      });
+    },
+    jumpTo: function (seconds) {
+      this.player.play();
+      this.player.currentTime(seconds);
+    },
+    formatDuration: function(duration) {
+      var hour = Math.floor(duration / 3600) || 0;
+      var minute = Math.floor((duration % 3600) / 60) || 0;
+      var second = Math.floor(duration % 60) || 0;
+      var sec_min = "";
+
+      if (hour > 0) {
+        sec_min += "" + hour + ":" + (minute < 10 ? "0" : "")
+      }
+      sec_min += "" + minute + ":" + (second < 10 ? "0" : "");
+      sec_min += "" + second;
+
+      return sec_min
+    },
+  }
 }
 
 // import { mapActions, mapGetters } from 'vuex';
@@ -94,5 +185,141 @@ export default {
 </script>
 
 <style lang="scss">
+// This is for overrding Video.js' default styles.
+// Video.js renders the element after the DOM is loaded.
+// Scoped styles won't work.
+.mv-universal-player.video-js:hover .vjs-big-play-button {
+  background-color: rgba(255, 255, 255, 0.75);
+  transition: background-color 0.3s linear;
+  border-color: transparent;
+}
 
+.mv-universal-player .vjs-big-play-button {
+  top: auto;
+  bottom: 30px;
+  left: 25px;
+  width: 70px;
+  height: 70px;
+  font-size: 3.5em;
+  line-height: 1.9;
+  border-radius: 50%;
+  border-color: transparent;
+  color: black;
+  background-color: white;
+  box-shadow: 0px 15px 30px rgba(0, 0, 0, 0.25);
+  transition: background-color 0.3s linear;
+}
+
+.vjs-control-bar {
+  font-size: 125%;
+}
+</style>
+
+<style scoped lang="scss">
+$light-grey: rgba(148, 148, 148, 0.7);
+$dark-grey: rgb(30, 30, 30);
+
+// Container that hosts both player and markers section.
+.mv-universal-player-container {
+  display: flex;
+  overflow: hidden;
+}
+
+// Player Section
+.mv-universal-player {
+  position: relative;
+  width: 100%;
+  flex-shrink: 0;
+  padding-top: 56.25%;
+}
+
+@media (min-width: 1000px) {
+  .mv-universal-player--with-markers {
+    width: 65%;
+    padding-top: 36.5625%;
+  }
+}
+
+// Markers Section
+.markers-container {
+  margin: 0;
+  flex-shrink: 0;
+  width: 100%;
+  min-height: 100%;
+  background-color: $dark-grey;
+  color: $light-grey;
+  font-family: sans-serif;
+  transform: translateX(-100%);
+}
+
+@media (min-width: 600px) {
+  .markers-container {
+    width: 50%;
+  }
+}
+
+@media (min-width: 1000px) {
+  .markers-container {
+    width: 35%;
+    transform: translateX(0);
+  }
+}
+
+.markers-header {
+  padding: 17.5px 21px;
+  border-bottom: 1px solid $light-grey;
+}
+
+.markers,
+.markers__item {
+  padding: 10px;
+}
+
+.markers {
+  list-style: none;
+  margin: 0;
+  height: 100%;
+  overflow-y: auto;
+}
+
+.markers__item {
+  display: flex;
+  align-items: center;
+  font-size: 0.875em;
+  cursor: pointer;
+  transition: color 0.4s, border-color 0.4s;
+  &:hover {
+    color: rgba(255, 255, 255, 0.9);
+    transition: color 0.4s, border-color 0.4s;
+  }
+}
+
+.markers__item:hover > .markers__time {
+  border-color: rgba(255, 255, 255, 0.9);
+}
+
+.markers__text,
+.markers__time {
+  vertical-align: middle;
+}
+
+.markers__text {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  text-align: center;
+}
+
+.markers__time {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 6px;
+  margin-right: 15px;
+  flex-shrink: 0;
+  width: 75px;
+  text-align: center;
+  border: 1px solid $light-grey;
+  border-radius: 5px;
+}
 </style>
