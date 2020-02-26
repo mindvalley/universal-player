cefd713b9085a49c613f762c2f09f7b1e1464845
Phidias Lo <aldenoah.zero@gmail.com>
2020-02-26 12:21:10 +0800

    Add test for video markers

--- tests/unit/player.spec.js
+++ tests/unit/player.spec.js
@@ -9,50 +9,94 @@ describe('Mindvalley Universal Player', () => {
   ]
 
   const markers = [
-    { time: 60, text: 'Marker Title 1' },
-    { time: 120, text: 'Marker Title 2' }
+    { time: 120, text: 'Marker Title 1' },
+    { time: 60, text: 'Marker Title 2' }
+  ]
+
+  const sorted_markers = [
+    { time: 60, text: 'Marker Title 2' },
+    { time: 120, text: 'Marker Title 1' }
   ]
 
   const wrapper = shallowMount(MindvalleyUniversalPlayer, {
-    propsData: { sources, markers }
+    propsData: { sources, markers },
+    data: function () {
+      return {
+        showMarkersMenu: false
+      }
+    }
   })
 
   const vm = wrapper.vm
 
-  // it('sources & markers should be object type', () => {
-  //   expect(typeof sources).toEqual('object')
-  //   expect(typeof markers).toEqual('object')
-  // })
+  vm.player.play = jest.fn()
+  vm.player.currentTime = jest.fn()
 
-  it('renders', () => {
-    expect(wrapper.exists()).toBe(true)
+  describe('Test Vue Methods', () => {
+    it('should run player currentTime when jumpTo is called', () => {
+      vm.jumpTo(60)
+      expect(vm.player.play).toBeCalled()
+      expect(vm.player.currentTime).toBeCalledWith(60)
+    })
+
+    it('should show showMarkersMenu true after toggleMarkers, if initial is false', () => {
+      vm.toggleMarkers()
+      expect(vm.showMarkersMenu).toBeTruthy()
+    })
+
+    it('should show showMarkersMenu false after toggleMarkers, if initial is true', () => {
+      wrapper.setData({ showMarkersMenu: true })
+      vm.toggleMarkers()
+      expect(vm.showMarkersMenu).toBeFalsy()
+    })
+
+    it('should formatDuration correctly', () => {
+      expect(vm.formatDuration(10)).toEqual('0:10')
+      expect(vm.formatDuration(60)).toEqual('1:00')
+      expect(vm.formatDuration(3600)).toEqual('1:00:00')
+    })
   })
 
-  it('should check if player is initilized', () => {
-    expect(vm.player).toBeTruthy()
+  describe('Test Vue Computed Properties', () => {
+    it('should show markersEnabled true when markers is not null', () => {
+      expect(vm.markersEnabled).toBeTruthy()
+    })
+
+    it('should show markersEnabled false when markers is null', () => {
+      wrapper.setProps({ markers: null })
+      expect(vm.markersEnabled).toBeFalsy()
+    })
+
+    it('should sort markers according to time', () => {
+      wrapper.setProps({ markers: markers })
+      expect(vm.sortedMarkers).toEqual(sorted_markers)
+    })
   })
 
-  it('should play when play button is clicked', () => {
-    const button = wrapper.find('.vjs-big-play-button')
-    vm.player.play = jest.fn()
-    button.trigger('click')
-    expect(vm.player.play).toBeCalled()
-  })
+  describe('video markers integration test', () => {
+    it('should render correctly when markers are not null', () => {
+      const marker_title = wrapper.find('.markers-header__title')
+      const marker_text = wrapper.find('.markers__text')
+      const marker_time = wrapper.find('.markers__time')
+      expect(marker_title.text()).toBe('Jump To')
+      expect(marker_text.text()).toBe('Marker Title 2')
+      expect(marker_time.text()).toBe('1:00')
+    })
 
-  it('should run player currentTime when jumpTo is called', () => {
-    vm.player.currentTime = jest.fn()
-    vm.jumpTo(60)
-    expect(vm.player.play).toBeCalled()
-    expect(vm.player.currentTime).toBeCalledWith(60)
-  })
+    it('should play video at the correct time when marker is clicked', () => {
+      const marker = wrapper.find('.markers__item')
+      marker.trigger('click')
+      expect(vm.player.play).toBeCalled()
+      expect(vm.player.currentTime).toBeCalledWith(60)
+    })
 
-  it('should toggleMarkers correctly', () => {
-    wrapper.setData({ showMarkersMenu: false })
-    vm.toggleMarkers()
-    expect(vm.showMarkersMenu).toBeTruthy()
-  })
+    it('should hide markers when close button is clicked', () => {
+      wrapper.setData({ showMarkersMenu: true })
 
-  it('should formatDuration correctly', () => {
-    expect(vm.formatDuration(3600)).toEqual('1:00:00')
+      const close_button = wrapper.find('.button--close')
+      const markers = wrapper.find('.markers-container')
+      close_button.trigger('click')
+      expect(vm.showMarkersMenu).toBeFalsy()
+    })
   })
 })
